b7720431334c23c43c0c6f85ae192aa6061dd2bb
Edvinas Sadeckas <edsdck@gmail.com>
2021-04-19 22:13:11 +0300

    Add rentals controller for CRUD operations

--- src/Rentals/Controllers/RentalsController.cs
+++ src/Rentals/Controllers/RentalsController.cs
@@ -1,23 +1,117 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rentals.Data;
+using Rentals.Data.Entities;
+using Rentals.Models.Dtos;
 
 namespace Rentals.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Authorize]
     public class RentalsController : ControllerBase
     {
-        [HttpGet]
-        public string Get()
+        private readonly RentalsContext _rentalsContext;
+
+        public RentalsController(RentalsContext rentalsContext)
         {
-            return "Rental-GET";
+            _rentalsContext = rentalsContext;
+        }
+
+        [HttpGet("{id:int:min(1)}")]
+        [ActionName(nameof(GetByIdAsync))]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(RentalDto), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<RentalDto>> GetByIdAsync(int id)
+        {
+            var rental = await _rentalsContext.Rentals
+                .SingleOrDefaultAsync(rent => rent.Id == id);
+
+            if (rental is null)
+            {
+                return NotFound();
+            }
+
+            var rentalDto = new RentalDto
+            {
+                Id = rental.Id,
+                Name = rental.Name,
+                Description = rental.Description,
+                Address = rental.Address
+            };
+            
+            return rentalDto;
         }
         
         [HttpPost]
-        public string Post(string text)
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public async Task<ActionResult> CreateAsync(RentalDto model)
         {
-            text += "-Rental";
+            var userId = User.FindFirstValue("sub");
             
-            return text;
+            var rental = new Rental
+            {
+                Name = model.Name,
+                Description = model.Description,
+                Address = model.Address,
+                OwnerId = userId
+            };
+
+            await _rentalsContext.Rentals.AddAsync(rental);
+            await _rentalsContext.SaveChangesAsync();
+            
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = rental.Id }, null);
+        }
+
+        [HttpPut]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public async Task<ActionResult> UpdateAsync(RentalDto model)
+        {
+            if (model.Id <= 0)
+            {
+                return NotFound("Please include rental id.");
+            }
+            
+            var rental = await _rentalsContext.Rentals
+                .SingleOrDefaultAsync(rent => rent.Id == model.Id);
+
+            if (rental is null)
+            {
+                return NotFound();
+            }
+
+            rental.Name = model.Name;
+            rental.Description = model.Description;
+            rental.Address = model.Address;
+
+            _rentalsContext.Rentals.Update(rental);
+            await _rentalsContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = rental.Id }, null);
+        }
+        
+        [HttpDelete("{id:int:min(1)}")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var rental = await _rentalsContext.Rentals
+                .SingleOrDefaultAsync(rent => rent.Id == id);
+
+            if (rental is null)
+            {
+                return NotFound();
+            }
+
+            _rentalsContext.Rentals.Remove(rental);
+            await _rentalsContext.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
---
+++ src/Rentals/Models/Dtos/RentalDto.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+using System.ComponentModel.DataAnnotations;
+
+namespace Rentals.Models.Dtos
+{
+    public class RentalDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+        
+        [Required]
+        public string? Address { get; set; }
+    }
+}
